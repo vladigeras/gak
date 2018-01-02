04728d39c81758d98fe90fadad89305902e024ed
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-01-02 12:34:54 +0300

    Swagger has been added

--- backend/src/main/java/ru/iate/gak/controller/MainController.java
+++ backend/src/main/java/ru/iate/gak/controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.iate.gak.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "mainController", description = "Test description of main controller")
 public class MainController {
 
     @GetMapping(path = "/hello")
