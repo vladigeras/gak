d142e5ccfe874c6e3d7ad1559ba6ce1fc0d5a377
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-03-21 14:58:40 +0300

    Fill statuses of diplom

--- backend/src/main/java/ru/iate/gak/domain/Status.java
+++ backend/src/main/java/ru/iate/gak/domain/Status.java
@@ -1,4 +1,10 @@
 package ru.iate.gak.domain;
 
 public enum Status {
+    ACTIVE,             // secretary doing that
+    SPEAKING_TIME,      // when president start a speaking process
+    SPEAKING_TIME_END,  // when president stop a speaking process
+    QUESTION_TIME,      // ...............start a question process
+    QUESTION_TIME_END,  // ...............stop a question process
+    DONE                // end of the speaking
 }
